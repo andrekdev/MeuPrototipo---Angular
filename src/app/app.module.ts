bd9b1da509421f9a441886326abaf59d9f48f5d9
andrek <zblackoez@gmail.com>
2023-03-29 01:41:41 -0400

    Adicionando componentes HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { HomeComponent } from './navegacao/inicio/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
